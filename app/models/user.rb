2fe96bfd4bbb730c2d60086d54dc47d8df5933b8
udagawa-hr4 <hiro619881022@gmil.com>
2021-01-18 16:26:30 +0900

    バリデーションの変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,22 +1,23 @@
 class User < ApplicationRecord
   
  
-  
-  validates :nickname, presence: true
+  with_options presence:true do
+    validates :nickname
+    validates :birthday
 
-  with_options format: { with: /\A[ぁ-んァ-ン一-龥]+\z/} do
-    validates :first_name, presence: true
-    validates :family_name, presence: true
-  end
-  
-  with_options format: { with: /\A[ァ-ヶー－]+\z/ } do
-    validates :read_first, presence: true
-    validates :read_family, presence: true
-  end
+    with_options format: { with: /\A[ぁ-んァ-ン一-龥]+\z/} do
+      validates :first_name
+      validates :family_name
+    end
+    
+    with_options format: { with: /\A[ァ-ヶー－]+\z/ } do
+      validates :read_first
+      validates :read_family
+    end
 
-  validates :birthday, presence: true
-  VALID_PASSWORD_REGEX =/\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
-  validates :password, format: { with: VALID_PASSWORD_REGEX },length: { minimum: 6}
+    VALID_PASSWORD_REGEX =/\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
+    validates :password, format: { with: VALID_PASSWORD_REGEX },length: { minimum: 6}
+  end
 
   has_many :items
   has_many :purchases
