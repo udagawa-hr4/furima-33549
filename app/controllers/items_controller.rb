7fe5202fbfa8ec476d1d881ae9fce82a6598639c
udagawa-hr4 <hiro619881022@gmil.com>
2021-01-20 17:56:39 +0900

    リダイレクトの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!,only: [:new]
+  before_action :authenticate_user!,only: [:new,:edit]
   def index
     @item = Item.includes(:user).order("created_at DESC")
   end
@@ -20,6 +20,10 @@ class ItemsController < ApplicationController
    end
    def edit
      @item = Item.find(params[:id])
+     unless user_signed_in? && current_user.id == @item.user.id
+      redirect_to action: :index
+    end
+
    end
    def update
     @item = Item.find(params[:id])
