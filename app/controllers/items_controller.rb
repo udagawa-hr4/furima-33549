74d05c0d3e9bc234b3f7ee669aca2821caebd127
udagawa-hr4 <hiro619881022@gmil.com>
2021-01-20 19:09:10 +0900

    コントローラーの編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!,only: [:new,:edit]
+  before_action :authenticate_user!,except: [:index,:show]
   before_action :find_params,only: [:show,:edit,:update]
   def index
     @item = Item.includes(:user).order("created_at DESC")
